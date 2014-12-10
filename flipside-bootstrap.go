4f97e741d95f4bc1f968793c1adc38fae2d70400
Nicola Peduzzi <thenikso@gmail.com>
2014-12-10 19:34:47 +0100

    Draft of bootstrap command

---
+++ flipside-bootstrap.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	// "github.com/mitchellh/goamz/aws"
+	// "github.com/mitchellh/goamz/ec2"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+)
+
+type Master struct {
+}
+
+func main() {
+	err := BootstrapAws("AKIAID3BWHBPAQEBJJ2Q", "6cV+ODCM3kwkZQ3ZJO6IKYQpOA89UldgvjIdAuv7")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Create Vagrantfile with default content
+// Run vagrant up
+func BootstrapVagrant() error {
+	os.MkdirAll(".flipside/salt/roots", 0755)
+	os.MkdirAll(".flipside/salt/pillar", 0755)
+	err := ioutil.WriteFile("Vagrantfile", []byte(`# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "ubuntu/trusty64"
+
+  config.vm.network "forwarded_port", guest: 80, host: 8090
+
+  config.vm.synced_folder ".flipside/salt/roots/", "/srv/salt/"
+  config.vm.synced_folder ".flipside/salt/pillar/", "/srv/pillar/"
+
+  config.vm.provision "shell", path: "flipside-provision.py", args: ["--salt-version", "git v2014.7.0", '--no-standalone']
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.customize ["modifyvm", :id, "--memory", "1024"]
+  end
+
+end`), 0644)
+	if err != nil {
+		return err
+	}
+	cmd := exec.Command("vagrant", "up")
+	cmd.Stdout = os.Stdout
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Check if aws instance is present
+// If not,
+// Setup Flipfile to use aws by default?
+func BootstrapAws(accessKey, secretKey string) error {
+	os.Mkdir(".flipside", 0755)
+	os.Chdir(".flipside")
+	err := ioutil.WriteFile("master.tf", []byte(`
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+    default = "eu-west-1"
+}
+variable "key_name" {}
+variable "key_path" {
+    default = "key_file.pem"
+}
+
+provider "aws" {
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+    region = "${var.region}"
+}
+
+resource "aws_security_group" "default" {
+    name = "flipside-elb"
+    description = "Used in the flipside terraform"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_instance" "flipside_master" {
+    ami = "ami-00b11177"
+    instance_type = "t1.micro"
+
+    connection {
+      user = "root"
+      key_file = "${var.key_path}"
+    }
+
+    key_name = "${var.key_name}"
+    security_groups = ["${aws_security_group.default.name}"]
+
+    provisioner "local-exec" {
+        command = "echo ${aws_instance.flipside_master.public_ip} > master.ip"
+    }
+
+    provisioner "remote-exec" {
+      script = "../flipside-provision.py"
+    }
+}
+
+resource "aws_eip" "ip" {
+    instance = "${aws_instance.flipside_master.id}"
+}
+
+output "ip" {
+    value = "${aws_eip.ip.public_ip}"
+}`), 0644)
+	if err != nil {
+		return err
+	}
+	// TODO generate keypair
+	err = ioutil.WriteFile("terraform.tfvars", []byte(fmt.Sprintf(`
+access_key = "%s"
+secret_key = "%s"
+key_name = "%s"
+key_path = "%s.pem"`, accessKey, secretKey, "flipside-dev", "flipside-dev")), 0644)
+	if err != nil {
+		return err
+	}
+	cmd := exec.Command("terraform", "plan")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ flipside-provision.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+'''Install and configure salt on the local machine.'''
+import subprocess
+import json
+import time
+import urllib.request
+import tempfile
+import os
+
+
+def install_salt(standalone, version='stable'):
+    '''Install Salt on the local machine'''
+    resp = urllib.request.urlopen('https://bootstrap.saltstack.com')
+    assert resp.status == 200
+    with tempfile.NamedTemporaryFile(mode='w+b') as f:
+        f.write(resp.read())
+        f.flush()
+        subprocess.check_call(
+            'sh {script} {opts} -n -p python-pip -p python-dev -p libtiff4-dev -p libjpeg8-dev -p zlib1g-dev -p libfreetype6-dev -p liblcms2-dev -p libwebp-dev -p libffi-dev -p libssl-dev -p cmake {version}'.format(
+                script=f.name,
+                opts='-X' if standalone else '-M -i local -A 127.0.0.1',
+                version=version
+            ).split()
+        )
+        if not subprocess.check_output('pip show pygit2'.split()):
+            subprocess.check_call(['bash', '-c', 'wget https://github.com/libgit2/libgit2/archive/v0.21.2.tar.gz && tar xzf v0.21.2.tar.gz && cd libgit2-0.21.2/ && cmake . && cmake build . && make install && ldconfig'], cwd='/tmp')
+            subprocess.check_call('pip install pygit2==0.21.4'.split())
+        else:
+            print('pygit2 already installed')
+
+
+def setup_salt(standalone):
+    # XXX Maybe we could simply use salt to... bootstrap salt
+    if not os.path.exists('/etc/salt/master.d'):
+        os.mkdir('/etc/salt/master.d')
+    if not os.path.exists('/etc/salt/minion.d'):
+        os.mkdir('/etc/salt/minion.d')
+    if not os.path.exists('/srv/salt/dist'):
+        os.mkdir('/srv/salt/dist')
+    if not os.path.exists('/srv/pillar'):
+        os.mkdir('/srv/pillar')
+    gitfs_config = 'fileserver_backend:\n  - roots\n  - git\n\ngitfs_remotes:\n  - https://github.com/saltstack-formulas/nginx-formula.git\n  - https://github.com/saltstack-formulas/users-formula.git\n  - https://github.com/saltstack-formulas/rsyslog-formula.git\n'
+    if standalone:
+        with open('/etc/salt/minion.d/local.config', 'w+') as f:
+            f.write('file_client:\n  local\n')
+        with open('/etc/salt/minion.d/flipside_gitfs.conf', 'w+') as f:
+            f.write(gitfs_config)
+    else:
+        with open('/etc/salt/master.d/flipside_gitfs.conf', 'w+') as f:
+            f.write(gitfs_config)
+        subprocess.check_call('sudo service salt-master restart'.split())
+        time.sleep(1)
+        subprocess.check_call('sudo salt-key -Ay'.split())
+
+
+def test_salt(standalone):
+    if standalone:
+        return
+    ping = {}
+    for i in range(2):
+        try:
+            ping = json.loads(
+                subprocess.check_output(
+                    'sudo salt * test.ping --out json --static'.split()
+                ).decode()
+            )
+        except subprocess.CalledProcessError:
+            pass
+    if ping.get('local'):
+        print('OK')
+    else:
+        raise SystemExit('ERROR')
+
+
+def highstate_salt(standalone):
+    subprocess.check_call('sudo salt-call state.highstate'.split())
+
+
+def main(standalone=True, highstate=True, salt_version=None):
+    install_salt(standalone, salt_version)
+    setup_salt(standalone)
+    test_salt(standalone)
+    if highstate:
+        highstate_salt(standalone)
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--no-standalone', action='store_true')
+    parser.add_argument('--no-highstate', action='store_true')
+    parser.add_argument('--salt-version', default='stable')
+    args = parser.parse_args()
+    main(standalone=not args.no_standalone, highstate=not args.no_highstate,
+         salt_version=args.salt_version)
