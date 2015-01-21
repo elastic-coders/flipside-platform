4d1cb244c25e7b0f7aa4620a45ad4cb53a963ac9
Marco Paolini <marco@credra.com>
2015-01-21 12:37:08 +0100

    Fix build of grunt_site apps

--- flipside_platform/build/grunt_build.py
+++ flipside_platform/build/grunt_build.py
@@ -11,4 +11,5 @@ from .. import config
 def build(dist_dir, environment, **kwargs):
     subprocess.check_call(['grunt', 'build:{}'.format(environment)])
     frontend_dir = os.path.join(dist_dir, 'frontend')
+    shutil.rmtree(frontend_dir)
     shutil.copytree('dist', frontend_dir)
