0c952dce7e48679caaff032950356b395a306cb4
Marco Paolini <marco@credra.com>
2015-01-23 23:24:09 +0100

    Utility for building regular python frontends

--- flipside_platform/build/python_wheel.py
+++ flipside_platform/build/python_wheel.py
@@ -44,3 +44,35 @@ def build(dist_dir, deps=True, use_requirements=False, py_version=2, ucs=4, **op
         else:
             subprocess.check_call([sys.executable, '-m', 'pip', 'wheel', '--wheel-dir', wheelhouse, '--find-links', wheelhouse, '--use-wheel', package_name])
         subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--find-links', wheelhouse, '--use-wheel', package_name])
+
+
+def build_django_frontend(dist_dir, frontend_dir='frontend', **kwargs):
+    import importlib
+    environ = kwargs.get('environment')
+    if os.path.exists(os.path.join(frontend_dir, 'bower.json')):
+        subprocess.check_call(
+            [
+                'bower', 'install'
+            ],
+            cwd=frontend_dir
+        )
+    if os.path.exists(os.path.join(frontend_dir, 'Gruntfile.js')):
+        subprocess.check_call(
+            [
+                'grunt',
+                'build{}'.format(':{}'.format(environ) if environ else '')
+            ],
+            cwd=frontend_dir
+        )
+    settings_module = kwargs.get('settings_module') or 'settings'
+    frontend_dist_dir = os.path.join(dist_dir, frontend_dir)
+    sys.path.insert(0, '.')
+    try:
+        settings = importlib.import_module(settings_module)
+    finally:
+        del sys.path[0]
+    if os.path.exists(frontend_dist_dir):
+        shutil.rmtree(frontend_dist_dir)
+    for _, src_path in settings.STATICFILES_DIRS:
+        shutil.copytree(src_path, os.path.join(frontend_dist_dir,
+                                               os.path.basename(src_path)))
